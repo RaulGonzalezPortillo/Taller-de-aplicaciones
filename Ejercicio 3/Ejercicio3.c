7e46733cfbfb800043916f20d2140c3a92f827ea
Raúl González Portillo <raulgonzalezportillo@gmail.com>
2019-10-03 11:07:06 -0500

    Examen 1 añadido

--- Ejercicio 3/Ejercicio3.c
+++ Ejercicio 3/Ejercicio3.c
@@ -20,7 +20,6 @@ void Enter ()
 
 void BuscarDireccionNodo (TipoNodo *Inicio, int Contador, int ConexionesMax, char IDBuscar, TipoNodo **NodoEncontrado)
 {
-  *NodoEncontrado = NULL;
   if (Inicio != NULL)
   {
     if (Inicio -> ID != IDBuscar)
@@ -49,62 +48,85 @@ void AnadirNodo (char *ID, TipoNodo **Inicio, int ConexionesMax)
   Nuevo = (TipoNodo *) malloc (sizeof (TipoNodo));
   Nuevo -> Cantidad = 0;
   Nuevo -> ID = *ID;
-  Nuevo -> Aristas = (TipoNodo **) malloc (sizeof (TipoNodo *) * 4);
+  Nuevo -> Aristas = (TipoNodo **) malloc (sizeof (TipoNodo *) * ConexionesMax);
   for (i = 0; i < ConexionesMax; i++)
     Nuevo -> Aristas [i] = NULL;
+  printf ("INFO: Agregando el nodo con el ID %c\n\n", Nuevo -> ID);
   if (*Inicio == NULL)
+  {
     *Inicio = Nuevo;
+    (*ID)++;
+  }
   else
   {
+    Error = 1;
     do
     {
       printf ("¿A donde quieres conectar este nodo?\n\n");
       __fpurge (stdin);
-      scanf ("%c", &IDPrevio);
+      scanf (" %c", &IDPrevio);
       printf ("\n");
+      Previo = NULL;
       BuscarDireccionNodo (*Inicio, 0, ConexionesMax, IDPrevio, &Previo);
       if (Previo != NULL)
       {
         printf ("INFO: Añadiendo al nodo con el ID: %c\n\n", Previo -> ID);
-        if (Previo -> Cantidad == 4)
-        {
+        if (Previo -> Cantidad == ConexionesMax - 1)
           printf ("INFO: El nodo está lleno\n\n");
-          Error = 1;
-          Enter ();
-        }
         else
         {
-          i = 0;
-          while (Previo -> Aristas [i] != NULL)
-            i++;
-          Previo -> Aristas [i] = Nuevo;
+          Previo -> Aristas [Previo -> Cantidad] = Nuevo;
           (Previo -> Cantidad)++;
+          printf ("INFO: Nodo añadido\n\n");
           Error = 0;
+          (*ID)++;
         }
       }
       else
-      {
         printf ("INFO: No se encontró el nodo\n\n");
-        Error = 1;
-        Enter ();
-      }
     }
     while (Error == 1);
   }
-  printf ("INFO: Nodo añadido\n\n");
   Enter ();
-  (*ID)++;
 }
 
-void ImprimirGrafo (TipoNodo *Inicio)
+void ImprimirGrafo (TipoNodo *Inicio, int Contador, int ConexionesMax)
 {
-  printf ("Ahí te va el grafo:\n\n");
-  /*while (Inicio != NULL)
+  if (Inicio != NULL)
   {
-    printf ("Nodo %c, conecciones activas: %d\n", Inicio -> ID, Inicio -> Cantidad);
-    Inicio = Inicio -> Sig;
+    while (Contador < ConexionesMax)
+    {
+      if (Inicio -> Aristas [Contador] != NULL)
+      {
+        ImprimirGrafo (Inicio -> Aristas [Contador], Contador, ConexionesMax);
+      }
+      if (Contador == ConexionesMax - 1)
+      {
+        printf ("%c\n\n", Inicio -> ID);
+      }
+      Contador++;
+    }
   }
-  printf ("\n");*/
+}
+
+void BorrarGrafo (TipoNodo *Inicio, int Contador, int ConexionesMax)
+{
+  if (Inicio != NULL)
+  {
+    while (Contador < ConexionesMax)
+    {
+      if (Inicio -> Aristas [Contador] != NULL)
+      {
+        BorrarGrafo (Inicio -> Aristas [Contador], Contador, ConexionesMax);
+      }
+      if (Contador == ConexionesMax - 1)
+      {
+        free (Inicio);
+        Inicio = NULL;
+      }
+      Contador++;
+    }
+  }
 }
 
 void Buscar (TipoNodo *Inicio, int ConexionesMax)
@@ -114,24 +136,52 @@ void Buscar (TipoNodo *Inicio, int ConexionesMax)
   TipoNodo *Buscador;
   printf ("Introduzca el ID del nodo a buscar\n\n");
   __fpurge (stdin);
-  scanf ("%c", &IDBuscador);
+  scanf (" %c", &IDBuscador);
   printf ("\n");
+  Buscador = NULL;
   BuscarDireccionNodo (Inicio, 0, ConexionesMax, IDBuscador, &Buscador);
   if (Buscador != NULL)
   {
     printf ("Se encontró el Nodo con el ID: %c\n\n", Buscador -> ID);
     printf ("Este nodo tiene %d conexiones:\n", Buscador -> Cantidad);
     for (i = 0; Buscador -> Aristas [i] != NULL; i++)
-    {
       printf ("Arista %d: Conectada al nodo %c\n", i + 1, Buscador -> Aristas [i] -> ID);
-    }
     printf ("\n");
-    Enter ();
   }
   else
   {
     printf ("No se encontró el nodo...\n\n");
-    Enter ();
+  }
+  Enter ();
+}
+
+void AnadirArista (TipoNodo *Inicio, int ConexionesMax)
+{
+  char IDOrigen, IDDestino;
+  TipoNodo *Origen, *Destino;
+  printf ("Introduzca el ID del nodo origen\n\n");
+  scanf (" %c", &IDOrigen);
+  printf ("Introduzca el ID del nodo destino\n\n");
+  scanf (" %c", &IDDestino);
+  Origen = NULL;
+  Destino = NULL;
+  BuscarDireccionNodo (Inicio, 0, ConexionesMax, IDOrigen, &Origen);
+  BuscarDireccionNodo (Inicio, 0, ConexionesMax, IDDestino, &Destino);
+  if (Origen == NULL || Destino == NULL)
+  {
+    printf ("No se encontró alguno de los nodos\n\n");
+  }
+  else
+  {
+    if (Origen -> Cantidad == ConexionesMax - 1)
+    {
+      printf ("El nodo origen está lleno\n\n");
+    }
+    else
+    {
+      Origen -> Aristas [Origen -> Cantidad] = Destino;
+      (Origen -> Cantidad)++;
+    }
   }
 }
 
@@ -140,6 +190,7 @@ int main (void)
   int ConexionesMax, Seleccion;
   char IDActual = 97;
   TipoNodo *Inicio = NULL;
+  system ("clear");
   printf ("Ingrese el número de conexiones máximas por Nodo:\n\n");
   scanf ("%d", &ConexionesMax);
   printf ("\n");
@@ -150,7 +201,7 @@ int main (void)
     printf ("2.- Añadir arista\n");
     printf ("3.- Desplegar gráfo\n");
     printf ("4.- Buscar dato\n");
-    printf ("5.- Salir\n\n");
+    printf ("5.- Salir y liberar memoria\n\n");
     scanf ("%d", &Seleccion);
     printf ("\n");
     switch (Seleccion)
@@ -159,12 +210,16 @@ int main (void)
       AnadirNodo (&IDActual, &Inicio, ConexionesMax);
       break;
       case 2:
+      AnadirArista (Inicio, ConexionesMax);
       break;
       case 3:
-      ImprimirGrafo (Inicio);
+      ImprimirGrafo (Inicio, 0, ConexionesMax);
       break;
       case 4:
       Buscar (Inicio, ConexionesMax);
+      break;
+      case 5:
+      BorrarGrafo (Inicio, 0, ConexionesMax);
       break;
     }
   } while (Seleccion != 5);
---
+++ Examen1/Examen1.c
@@ -0,0 +1,104 @@
+//Este programa regenera una señal por el método de Fourier mediante los datos de su
+//Amplitud, frecuencia, número de harmónicos y tiempo final los cuales ingresa el
+//usuario dentro del programa
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct def_Senal //Estructura de la señal, n = número de harmónicos, A = Amplitud, f = frecuencia, tf = tiempo final, t = Arreglo de t, ft = Arreglo de f(t)
+{
+  int n;
+  double A, f, tf;
+  double *t, *ft;
+  struct def_Senal *Sig;
+} TipoSenal;
+
+void PedirDatos (double *A, double *f, int *n, double *tf, int *sizeoft) //Esta función le pide al usuario los datos de la señal y los devuelve al main
+{
+  printf ("Introduzca los sigs. datos\n\n");
+  printf ("Amplitud\n\n");
+  scanf ("%lf", A);
+  printf ("\n");
+  printf ("Frecuencia\n\n");
+  scanf ("%lf", f);
+  printf ("\n");
+  printf ("Número de harmónicos\n\n");
+  scanf ("%d", n);
+  printf ("\n");
+  printf ("Tiempo final\n\n");
+  scanf ("%lf", tf);
+  printf ("\n");
+  printf ("Tamaño de T\n\n");
+  scanf ("%d", sizeoft);
+  printf ("\n");
+}
+
+void Instanciar (double A, double f, int n, double tf, int sizeoft, TipoSenal *Senal) //Esta función instancia la estructura de la señal, asignando los valores del usuario y reservando la memoria necesaria
+{
+  Senal -> A = A;
+  Senal -> f = f;
+  Senal -> n = n;
+  Senal -> tf = tf;
+  Senal -> t = (double *) malloc (sizeof (double) * sizeoft);
+  Senal -> ft = (double *) malloc (sizeof (double) * sizeoft);
+}
+
+void Resolver (double sizeoft, TipoSenal *Senal) //Esta función recibe el tamaño de los arreglos y la señal instanciada, procede a resolver por Fourier
+{
+  int i;
+  double j;
+  double Incremento = Senal -> tf / sizeoft;
+  double Sumatoria;
+  for (i = 0; i <= sizeoft; i++)
+  {
+    Senal -> t[i] = Incremento * (i);
+    //printf (" %lf", Senal -> t[i]);
+    Sumatoria = 0;
+    for (j = 1; j <= Senal -> n; j++)
+    {
+      Sumatoria += (j - ((Senal -> A / (M_PI * j)) * (cos (M_PI * j) - 1) * (sin (2 * M_PI * j * Senal -> f * Senal -> t[i]))));
+      //printf (" %lf", Sumatoria);
+    }
+    Senal -> ft[i] = Sumatoria + (Senal -> A / 2);
+  }
+}
+
+void ImprimirArch (double sizeoft, TipoSenal Senal) //Esta función recorre los arreglos de t y de f(t) y guarda los datos en un archivo
+{
+  int i;
+  FILE *Archivo;
+  printf ("\n");
+  Archivo = fopen ("DatosFourier.txt", "wt");
+  for (i = 0; i < sizeoft; i++)
+  {
+    fprintf (Archivo, "%lf %lf\n", Senal.t [i], Senal.ft [i]);
+  }
+  fclose (Archivo);
+}
+
+void Graficar() //Abre GNU plot, le envía los datos registrados y los grafica, añadiendole título a los ejes y a la gráfica
+{
+  int i;
+  char *AbrirGnuPlot[] = {"set title \"Método de Fourier\"",
+			  "set ylabel \"----f(t)--->\"",
+			  "set xlabel \"----t--->\"",
+			  "plot \"DatosFourier.txt\" with lines"
+  };
+  FILE *VentanaGnuPlot = popen ("gnuplot -persist", "w");
+  for (i=0; i<4; i++)
+    fprintf(VentanaGnuPlot, "%s \n", AbrirGnuPlot[i]);
+}
+
+int main (int argc, char *argv[])
+{
+  int n, sizeoft;
+  double A, f, tf;
+  TipoSenal Senal;
+  PedirDatos (&A, &f, &n, &tf, &sizeoft);
+  Instanciar (A, f, n, tf, sizeoft, &Senal);
+  Resolver (sizeoft, &Senal);
+  ImprimirArch (sizeoft, Senal);
+  Graficar ();
+}
